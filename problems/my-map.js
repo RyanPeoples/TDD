0479ed611fe72f5f0f9d8a306ea24253c485e66d
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-10 15:40:59 -0700

    phase 3 done

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,12 @@
 function myMap(inputArray, callback) {
-  // Your code here
+  let arr = [];
+
+  for(let i = 0; i< inputArray.length;i++){
+    let ele = inputArray[i];
+    arr.push(callback(ele));
+  }
+  
+  return arr;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,31 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+
+const myMap = require('../problems/my-map');
+
+describe("myMap(inputArray, callback", () => {
+    let newArr;
+    let callback;
+    beforeEach(()=>{
+        newArr = [1,2,3]
+        callback = (el) => el * 2;
+    })
+    it("Should not mutate original array", ()=>{
+        const res = myMap(newArr, callback);
+        expect(newArr).to.equal(newArr);
+    });
+
+    it("Should not call built in Array.map()",()=>{
+        const mapSpy = chai.spy.on(newArr, "map");
+        myMap(newArr, callback);
+        expect(mapSpy).to.have.not.been.called();
+    });
+
+    it("Should invoke passed in callback for each array element",()=>{
+        const mapSpy = chai.spy(callback);
+        myMap(newArr, mapSpy);
+        expect(mapSpy).to.have.been.called(newArr.length);
+    });
+})
